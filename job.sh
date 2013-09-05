a79eb42387d8fd3a1e49dd17d7bba45ed5620322
Daz Jones <yuki@thebronasium.com>
2013-09-05 15:36:10 +0100

    job.sh: move source trees out of the workspace
    This reverts commit 16932afda4da862745c55c7fd45f3e063533272e.

--- job.sh
+++ job.sh
@@ -4,6 +4,11 @@ then
   export HOME=$(awk -F: -v v="$USER" '{if ($1==v) print $6}' /etc/passwd)
 fi
 
+cd $WORKSPACE
+mkdir -p ../android
+cd ../android
+export WORKSPACE=$PWD
+
 if [ ! -d jenkins ]
 then
   git clone git://github.com/Dazzozo/jenkins.git
