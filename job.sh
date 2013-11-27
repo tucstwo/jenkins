049f5ea2fd5ee8301243d85b3d89164b9cf6efba
cameron581 <cameron581@cox.net>
2013-11-26 18:57:14 -0600

    Update job.sh

--- job.sh
+++ job.sh
@@ -3,11 +3,6 @@ mkdir -p ../android
 cd ../android
 export WORKSPACE=$PWD
 
-if [ ! -d jenkins ]
-then
-  git clone git://github.com/fusion2/jenkins.git
-fi
-
 cd jenkins
 ## Get rid of possible local changes
 git reset --hard
