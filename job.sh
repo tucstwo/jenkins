83b1d8ee7b0bed72dc32aaa086dd6bb1a293d07b
cameron581 <cameron581@cox.net>
2013-11-26 19:03:54 -0600

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,1 +1,12 @@
-exec ./build.sh
+cd $WORKSPACE
+mkdir -p ../android
+cd ../android
+export WORKSPACE=$PWD
+
+cd jenkins
+## Get rid of possible local changes
+git reset --hard
+git pull -s resolve
+
+sudo chmod +x ./build.sh
+sudo ./build.sh
