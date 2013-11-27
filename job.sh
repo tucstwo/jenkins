6a9b1e8ad798f2fd42665d4b7896c80c0906c464
cameron581 <cameron581@cox.net>
2013-11-26 19:19:04 -0600

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,7 +1,14 @@
+if [ -z "$HOME" ]
+then
+  echo HOME not in environment, guessing...
+  export HOME=$(awk -F: -v v="$USER" '{if ($1==v) print $6}' /etc/passwd)
+fi
+
 cd $WORKSPACE
 mkdir -p ../android
 cd ../android
 export WORKSPACE=$PWD
+
 
 cd jenkins
 ## Get rid of possible local changes
